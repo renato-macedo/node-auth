1bf02ec9d657239098254e668ef560fcb350810d
Renato <renatomaceneto@gmail.com>
2019-12-27 21:54:28 -0300

    config

---
+++ api/src/config/index.ts
@@ -0,0 +1,4 @@
+export * from './app';
+export * from './db';
+export * from './cache';
+export * from './session';
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,31 @@
-import express from 'express'
+import express from 'express';
+import Redis from 'ioredis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import mongoose from 'mongoose';
+import {
+  REDIS_OPTIONS,
+  MONGO_URI,
+  MONGO_OPTIONS,
+  SESSION_OPTIONS,
+  APP_PORT
+} from './config';
 
-const app = express()
+(async () => {
+  await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
+  const RedisStore = connectRedis(session);
 
-app.get('/', (req, res) => res.json({ message: 'works' }))
+  const client = new Redis(REDIS_OPTIONS);
 
-app.listen(3000, () => console.log(`http://localhost:${3000}`))
+  const app = express();
+  app.use(
+    session({
+      ...SESSION_OPTIONS,
+      store: new RedisStore({ client })
+    })
+  );
+
+  app.get('/', (req, res) => res.json({ message: 'works' }));
+
+  app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}`));
+})();
